86dd0dbe10bf45633691ec2b8f285f62be0f4085
Harishankar G <harishankargaddanakeri@gmail.com>
2025-05-05 13:42:59 +0530

    Add connection for remote db

--- config/config.go
+++ config/config.go
@@ -1,28 +1,43 @@
 package config
 
 import (
-	"errors"
 	"log"
 	"os"
+	"path/filepath"
 )
 
 var (
 	DatabaseURL string
 	JWTSecret   string
+	AivenPGCert string
 )
 
 func LoadEnv() error {
 	DatabaseURL = os.Getenv("DATABASE_URL")
 	if DatabaseURL == "" {
 		log.Fatal("DATABASE_URL is not set in environment")
-		return errors.New("DATABASE_URL is not set in environment")
 	}
 
 	JWTSecret = os.Getenv("JWT_SECRET")
 	if JWTSecret == "" {
 		log.Fatal("JWT_SECRET is not set in environment")
-		return errors.New("JWT_SECRET is not set in environment")
+	}
+
+	AivenPGCert = os.Getenv("AIVEN_PG_CERT")
+	if AivenPGCert == "" {
+		log.Fatal("AIVEN_PG_CERT is not set in environment")
+	}
+	if err := writeCertToFile(); err != nil {
+		log.Fatalf("Unable to write AIVEN_PG_CERT to disk: %v", err)
 	}
 
 	return nil
+}
+
+func writeCertToFile() error {
+	tmpDir := os.TempDir()
+	certPath := filepath.Join(tmpDir, "ca.pem")
+	err := os.WriteFile(certPath, []byte(AivenPGCert), 0600)
+
+	return err
 }
