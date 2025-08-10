1e021f3f3dc0311c9d5eac00ffafa0a54e70d66c
Harishankar G <harishankargaddanakeri@gmail.com>
2025-08-10 22:59:01 +0530

    Handle db error where the username or email was already used by someone else

--- internal/repository/common.go
+++ internal/repository/common.go
@@ -3,5 +3,6 @@ package repository
 import "errors"
 
 var (
-	ErrorDatabase = errors.New("error while interacting with the database")
+	ErrorDatabase   = errors.New("error while interacting with the database")
+	ErrorUserExists = errors.New("user with username or email already exists")
 )
--- internal/repository/user_repo.go
+++ internal/repository/user_repo.go
@@ -7,14 +7,26 @@ import (
 	"log/slog"
 
 	"github.com/google/uuid"
+	"github.com/jackc/pgx/v5/pgconn"
 )
 
 func CreateUser(ctx context.Context, user model.UserSignUp) error {
 	db := database.New()
 	query := `INSERT INTO users (id, username, email, password_hash, created_at) VALUES ($1, $2, $3, $4, now())`
 	if _, err := db.ExecContext(ctx, query, uuid.New(), user.Username, user.Email, user.Password); err != nil {
-		slog.Error("create user: ", slog.Any("error", err))
-		return ErrorDatabase
+		error, _ := err.(*pgconn.PgError)
+		switch error.Code {
+		case "23505":
+			{
+				slog.Info(error.Error())
+				return ErrorUserExists
+			}
+		default:
+			{
+				slog.Error("c", slog.Any("error", err))
+			}
+
+		}
 	}
 
 	return nil
