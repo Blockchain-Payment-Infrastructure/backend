b8e1a995b0b1157e8ae4570f739e196b9143b416
Harishankar G <harishankargaddanakeri@gmail.com>
2025-09-12 17:13:23 +0530

    Add login swagger documentation
    (cherry picked from commit edae9b64234d28e1ae0951b49d4ef81d9b70c0a3)

--- internal/api/handler/auth_handler.go
+++ internal/api/handler/auth_handler.go
@@ -36,6 +36,18 @@ func SignUpHandler(c *gin.Context) {
 	c.JSON(http.StatusCreated, gin.H{"result": "account created successfully"})
 }
 
+// LoginHandler godoc
+//
+//	@Summary		User Login
+//	@Description	Logs the user in and returns a jwt token
+//	@Tags			auth
+//	@Accept			json
+//	@Produce		json
+//	@Param			loginDetails	body		model.UserLogin	true	"User login details"
+//	@Success		200				{string}	string			"Login successful!"
+//	@Failure		400				{string}	string			"Validation error"
+//	@Failure		500				{string}	string			"Internal server error"
+//	@Router			/auth/login [post]
 func LoginHandler(c *gin.Context) {
 	var loginDetails model.UserLogin
 
